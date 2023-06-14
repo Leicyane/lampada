c66216731d6181dbb2470d66e12c4c7d905ca6ad
leicyane <leicyanen@gmail.com>
2023-06-13 21:40:03 -0500

    projetolampada

---
+++ lampada.js
@@ -0,0 +1,28 @@
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp =  document.getElementById('lamp');
+
+function  isLampBroken () {
+    return lamp.src.indexOf ('quebrada') > -1
+
+}
+
+function lampOn () {
+    if (!isLampBroken () ) {  
+    lamp.src = './img/ligada.jpg';
+    }
+}
+function lampOff (){
+    if (!isLampBroken()){
+    lamp.src='./img/desligada.png';
+    }
+}
+function lampBroken (){
+    lamp.src= './img/quebrada.png';
+}
+
+turnOn.addEventListener ('click',lampOn);
+turnOff.addEventListener('click',lampOff);
+lamp.addEventListener ( 'mouseover',lampOn);
+lamp.addEventListener ('mouseleave',lampOff );
+lamp.addEventListener ('dblclick', lampBroken);
